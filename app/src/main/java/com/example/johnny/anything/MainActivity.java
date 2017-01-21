8ee23f3daa8cd220ac1b50bc2790630208ae0213
samjjc <sam111jc@gmail.com>
2017-01-21 12:22:47 -0500

    camera function

--- app/src/main/java/com/example/johnny/anything/MainActivity.java
+++ app/src/main/java/com/example/johnny/anything/MainActivity.java
@@ -3,38 +3,35 @@ package com.example.johnny.anything;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+//    ImageView mImageView= (ImageView) findViewById(R.id.image);
+//    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView mimageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mimageView = (ImageView) this.findViewById(R.id.image_from_camera);
+        Button button = (Button) this.findViewById(R.id.take_image_from_camera);
     }
 
-    public void getImage(View view){
-        dispatchTakePictureIntent();
-
-
+    public void takeImageFromCamera(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap mphoto = (Bitmap) data.getExtras().get("data");
+            mimageView.setImageBitmap(mphoto);
         }
     }
 
