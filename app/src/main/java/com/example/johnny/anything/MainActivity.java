437f9c38d9cb4ef682566e7bacf2775be062d501
samjjc <sam111jc@gmail.com>
2017-01-21 17:50:14 -0500

    added for loop

--- app/src/main/java/com/example/johnny/anything/MainActivity.java
+++ app/src/main/java/com/example/johnny/anything/MainActivity.java
@@ -9,7 +9,6 @@ import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextBlock;
@@ -21,15 +20,25 @@ public class MainActivity extends AppCompatActivity {
     private static final int CAMERA_REQUEST = 1888;
     ImageView mimageView;
     TextRecognizer textRecognizer;
+    Bitmap mphoto;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mimageView = (ImageView) this.findViewById(R.id.image_from_camera);
+        if(mphoto!= null){mimageView.setImageBitmap(mphoto);}
         Button button = (Button) this.findViewById(R.id.take_image_from_camera);
         textRecognizer = new TextRecognizer.Builder(this).build();
     }
+
+    protected void onPause() {
+        super.onPause();
+        if(mphoto!= null){mimageView.setImageBitmap(mphoto);}
+
+
+    }
+
 
     public void takeImageFromCamera(View view) {
         Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
@@ -38,12 +47,19 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap mphoto = (Bitmap) data.getExtras().get("data");
+            mphoto = (Bitmap) data.getExtras().get("data");
             Frame outputFrame = new Frame.Builder().setBitmap(mphoto).build();
             textRecognizer.detect(outputFrame);
             SparseArray<TextBlock> foo = findText(outputFrame);
-            output = foo.get(0).getValue();
-            Log.d("MAIN", output);
+            if(foo.get(0)!=null) {
+                for(int i =0;i<foo.size();i++) {
+                    output = foo.get(i).getValue();
+                    Log.d("MAIN", output);
+                }
+            }
+            else{
+                Log.d("MAIN", "NOOOOOOOOOOOOOOOO TEXTTTTTTTT");
+            }
             mimageView.setImageBitmap(mphoto);
         }
     }
