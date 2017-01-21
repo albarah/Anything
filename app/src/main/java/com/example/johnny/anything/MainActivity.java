7af0ea9e0090e65441921c9b66f0732bc0ef13ba
samjjc <sam111jc@gmail.com>
2017-01-21 14:45:32 -0500

    implemented textrecognizer

--- app/src/main/java/com/example/johnny/anything/MainActivity.java
+++ app/src/main/java/com/example/johnny/anything/MainActivity.java
@@ -1,24 +1,22 @@
 package com.example.johnny.anything;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity {
-//    ImageView mImageView= (ImageView) findViewById(R.id.image);
-//    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private static final int CAMERA_REQUEST = 1888;
     ImageView mimageView;
+    TextRecognizer textRecognizer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,11 +24,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mimageView = (ImageView) this.findViewById(R.id.image_from_camera);
         Button button = (Button) this.findViewById(R.id.take_image_from_camera);
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(this).build();
+        textRecognizer = new TextRecognizer.Builder(this).build();
         if (textRecognizer.isOperational()==true){
             TextView thing = (TextView) findViewById(R.id.Viral);
             thing.setText("SHIT'S LIT FAM");
         }
+
     }
 
     public void takeImageFromCamera(View view) {
@@ -41,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             Bitmap mphoto = (Bitmap) data.getExtras().get("data");
+            Frame outputFrame = new Frame.Builder().setBitmap(mphoto).build();
+            textRecognizer.detect(outputFrame);
             mimageView.setImageBitmap(mphoto);
         }
     }
